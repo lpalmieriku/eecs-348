5ddfa514e5d9886ac868ac26a65d3b14c3a4183f
lpalmieriku <l250p415@home.ku.edu>
2024-09-04 09:20:54 -0500

    Add hello-world program

---
+++ lab1/hello-world.cpp
@@ -0,0 +1,16 @@
+/*
+Author: Lena Palmieri
+KUID: 3115099
+Date: 9/4/24
+Lab: lab01
+Last Modified: 9/4/24
+Purpose: Learning the basics of Git and Github.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+  return 0;
+}
